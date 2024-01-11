f7b4572a05f522f424014bfbb8c47fc4e8cdfd65
jkh675 <rluo2018@gmail.com>
2024-01-11 22:58:42 +0800

    impl add shooter dialog

--- src/app/scorelists/[id]/page.tsx
+++ src/app/scorelists/[id]/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { gql, useQuery } from "@apollo/client";
-import { Box, Card, Divider, Grid, IconButton, PaperProps, SpeedDial, SpeedDialAction, SpeedDialIcon, Stack, Typography, TypographyProps, styled, useTheme } from "@mui/material";
+import { Box, Button, Card, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, FormControl, Grid, IconButton, InputLabel, MenuItem, PaperProps, Select, SpeedDial, SpeedDialAction, SpeedDialIcon, Stack, Typography, TypographyProps, styled, useTheme } from "@mui/material";
 import { Query, Score } from "@/gql_dto";
 import React from "react";
 import { DataGrid, GridColDef, GridValueGetterParams, useGridApiRef } from '@mui/x-data-grid';
 import { Delete, Edit, PeopleOutlined, PersonAdd, Queue } from "@mui/icons-material";
-
+import AddShooterDialog from "./addShooterDialog"
 
 const GET_SCORELIST_QUERY = gql`
     query GetScorelist($id:Int!){
@@ -37,6 +37,7 @@ const GET_SCORELIST_QUERY = gql`
         }
     }
 `
+
 const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
     position: 'absolute',
     '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
@@ -53,16 +54,12 @@ export default function ScorelistPage({ params }: { params: { id: string } }) {
     const id = parseInt(params.id);
     const scorelist = useQuery<Query>(GET_SCORELIST_QUERY, { variables: { id } });
 
-    if (scorelist.loading)
-        return <pre>Loading...</pre>
-    if (scorelist.error)
-        return <pre>Error: {JSON.stringify(scorelist.error)}</pre>
-    if (!scorelist.data)
-        return <pre>No Data</pre>
 
 
+    const [dialogOpen, setDialogOpen] = React.useState(false);
 
     const theme = useTheme();
+    // #region collumn def
     interface ActionButtonProps {
         row: Score;
     }
@@ -202,7 +199,14 @@ export default function ScorelistPage({ params }: { params: { id: string } }) {
             },
         },
     ];
+    // #endregion
 
+    if (scorelist.loading)
+        return <pre>Loading...</pre>
+    if (scorelist.error)
+        return <pre>Error: {JSON.stringify(scorelist.error)}</pre>
+    if (!scorelist.data)
+        return <pre>No Data</pre>
     return (
         <>
             <Stack divider={<Divider />} gap={2}>
@@ -230,13 +234,22 @@ export default function ScorelistPage({ params }: { params: { id: string } }) {
                     icon={<PersonAdd />}
                     tooltipOpen
                     tooltipTitle={"Add shooter"}
+                    onClick={() => {
+                        setDialogOpen(true)
+                    }}
                 />
                 <SpeedDialAction
                     icon={<Queue />}
                     tooltipOpen
-                    tooltipTitle={"Add rounds"}
+                    tooltipTitle={"Add round"}
                 />
             </StyledSpeedDial>
+            <Dialog
+                open={dialogOpen}
+                onClose={() => setDialogOpen(false)}
+            >
+                <AddShooterDialog onClose={() => setDialogOpen(false)} scorelistId={id} />
+            </Dialog>
         </>
     )
 }
